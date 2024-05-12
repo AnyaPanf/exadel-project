183c543076db838fe72f4f30d1f404565ec9ad40
Anna Panferova <anyapanf@gmail.com>
2024-05-12 16:01:01 +0600

    files are uploaded and downloaded successfully

--- src/routes/Files.tsx
+++ src/routes/Files.tsx
@@ -5,7 +5,7 @@ export const Files = () => {
   const [shouldRerender, setShouldRerender] = useState<boolean>(false);
 
   useEffect(() => {
-    fetch('http://localhost:3000/')
+    fetch('http://localhost:3000/list')
       .then(response => response.json())
       .then(data => setAllDocs(data));
   }, [shouldRerender]);
@@ -13,28 +13,20 @@ export const Files = () => {
   const handleDownload = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     const docName = e.currentTarget.name;
 
-    function downloadFile(url: string, fileName: string) {
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = fileName;
-      a.style.display = 'none';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-    }
-
-    await fetch(`http://localhost:3000//public/files/${docName}`)
+    await fetch(`http://localhost:3000/download/${docName}`)
       .then((response) => response.blob())
       .then((blob) => {
-        const url = window.URL.createObjectURL(blob);
-        console.log(url);
-        
-        downloadFile(url, docName); 
-        window.URL.revokeObjectURL(url);
+        let url = URL.createObjectURL(blob);
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = docName;
+        a.click();
       });
   }
 
   const handleDelete = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const getDeleted = confirm('Are you sure you want to delete this document?');
+    if (!getDeleted) return;
     const docId = e.currentTarget.value;
     const docName = e.currentTarget.name;
     const res = await fetch(`http://localhost:3000/${docId}/${docName}`, {
@@ -51,20 +43,22 @@ export const Files = () => {
         <div className="files">
           <table className="files_table">
             <thead>
-              <th>ID</th>
-              <th>File name</th>
-              <th>Date</th>
-              <th colSpan={2}>Actions</th>
+              <tr>
+                <th>ID</th>
+                <th>File name</th>
+                <th>Date</th>
+                <th colSpan={2}>Actions</th>
+              </tr>
             </thead>
             <tbody>
               {allDocs.map((doc) => {
                 const { id, name, created_at } = doc
                 return (
-                  <tr className="files_tablerow">
+                  <tr key={id} className="files_tablerow">
                     <td>{id}</td>
                     <td>{name}</td>
                     <td>{created_at.slice(0, 10)}</td>
-                    <td><button type="submit" onClick={handleDownload} className="files_btn">Download</button></td>
+                    <td><button type="submit" name={name} onClick={handleDownload} className="files_btn">Download</button></td>
                     <td><button value={id} name={name} type="submit" onClick={handleDelete} className="files_btn">Delete</button></td>
                   </tr>
                 )
--- src/routes/Upload.tsx
+++ src/routes/Upload.tsx
@@ -1,14 +1,19 @@
 import { FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Upload = () => {
+    const navigate = useNavigate();
     const handleUpload = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const currentTarget = e.currentTarget;
         const formData = new FormData(currentTarget);
-        await fetch('http://localhost:3000/', {
+        const res = await fetch('http://localhost:3000/', {
             method: "POST",
             body: formData,
-        })
+        });
+        if (res.ok) {
+            navigate('/files');;
+        }
         currentTarget.reset();
     }
 
@@ -23,6 +28,7 @@ export const Upload = () => {
                     id="file"
                     type="file"
                     className="upload_input"
+                    required
                 />
                 <div className="btn">
                     <button
